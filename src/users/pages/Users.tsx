a901a039a0eb72472cd6878f2bcd217f7e09e835
diegoandres981 <diegoacallejonc@gmail.com>
2021-04-21 21:02:59 -0300

    Dev continuation

--- src/users/pages/Users.tsx
+++ src/users/pages/Users.tsx
@@ -3,10 +3,11 @@ import "./Users.css";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [
+  const USERS: any = [
     {
       id: "u1",
-      image: "default_image",
+      image:
+        "https://i.picsum.photos/id/877/400/300.jpg?hmac=qD6pLIhs_M4_LL-K2MYUuyd89Dzqfo1iNVEM7CyYScs",
       name: "default_user",
       placeCount: 1,
     },
